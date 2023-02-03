bed6bf769e4ff502526254b2720c07ab7933bcf0
Coen Steuns <c.steuns@gmail.com>
2023-02-03 18:27:58 +0100

    uppercase

--- Assets/Scripts/BonkIncStandard/Utils/Static/Extensions/TransformExtention.cs
+++ Assets/Scripts/BonkIncStandard/Utils/Static/Extensions/TransformExtention.cs
@@ -2,63 +2,63 @@ using UnityEngine;
 
 public static class TransformExtensions
 {
-    public static void setPositionX(this Transform transform, float x)
+    public static void SetPositionX(this Transform transform, float x)
     {
         var position = transform.position;
         position.x = x;
         transform.position = position;
     }
 
-    public static void setPositionY(this Transform transform, float y)
+    public static void SetPositionY(this Transform transform, float y)
     {
         var position = transform.position;
         position.y = y;
         transform.position = position;
     }
 
-    public static void setPositionZ(this Transform transform, float z)
+    public static void SetPositionZ(this Transform transform, float z)
     {
         var position = transform.position;
         position.z = z;
         transform.position = position;
     }
 
-    public static void setLocalX(this Transform transform, float x)
+    public static void SetLocalPositionX(this Transform transform, float x)
     {
         var position = transform.localPosition;
         position.x = x;
         transform.localPosition = position;
     }
 
-    public static void setLocalY(this Transform transform, float y)
+    public static void SetLocalPositionlY(this Transform transform, float y)
     {
         var position = transform.localPosition;
         position.y = y;
         transform.localPosition = position;
     }
 
-    public static void setLocalZ(this Transform transform, float z)
+    public static void SetLocalPositionZ(this Transform transform, float z)
     {
         var position = transform.localPosition;
         position.z = z;
         transform.localPosition = position;
     }
 
-    public static void setLocalScaleX(this Transform transform, float x)
+    public static void SetLocalScaleX(this Transform transform, float x)
     {
         var scale = transform.localScale;
         scale.x = x;
         transform.localScale = scale;
     }
 
-    public static void setLocalScaleY(this Transform transform, float y)
+    public static void SetLocalScaleY(this Transform transform, float y)
     {
         var scale = transform.localScale;
         scale.y = y;
         transform.localScale = scale;
     }
 
-    public static void setLocalScaleZ(this Transform transform, float z)
+    public static void SetLocalScaleZ(this Transform transform, float z)
     {
         var scale = transform.localScale;
         scale.z = z;
