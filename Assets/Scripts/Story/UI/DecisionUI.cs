f0aaa893ed5aec343ffe876679cb2289db974bc2
Traxx186 <justinvanderkruit@icloud.com>
2023-02-05 11:36:58 +0100

    added animation to button

--- Assets/Scripts/Story/UI/DecisionUI.cs
+++ Assets/Scripts/Story/UI/DecisionUI.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DecisionUI : MonoBehaviour
 {
-
     [SerializeField]
     private TMPro.TextMeshProUGUI titleUI;
 
@@ -28,6 +25,4 @@ public class DecisionUI : MonoBehaviour
     {
         OnChosen?.Invoke(Decision);
     }
-
-
 }
